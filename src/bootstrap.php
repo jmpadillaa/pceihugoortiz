df7aef0f6e9b997e73a0d435f1ad8ce5e1ce8e1e
santiago <san566@outlook.com>
2023-07-15 11:15:17 -0500

    Sesión con memcached

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,6 +4,13 @@ require_once './app/Router.php';
 
 ini_set('session.name', 'sessionid');
 
+if (defined('APP_MEMCACHED_HOST')) {
+    ini_set('session.save_handler', 'memcached');
+    ini_set('session.save_path', APP_MEMCACHED_HOST);
+    ini_set('memcached.sess_prefix', 'memc.inscripcion.');
+    ini_set('memcached.sess_lock_expire', 7200);
+}
+
 session_start();
 
 date_default_timezone_set('America/Guayaquil');
--- src/config.php
+++ src/config.php
@@ -9,3 +9,5 @@ define('APP_DB_NAME', 'pcei');
 define('APP_DB_USER', 'postgres');
 
 define('APP_DB_PWD', 'postgres');
+
+define('APP_MEMCACHED_HOST', '127.0.0.1:11211');
